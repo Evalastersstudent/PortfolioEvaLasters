0321e4790040f2763d6c677fd4f11272b6a0be1e
EVA LASTERS <EVA.LASTERS@STUDENT.AP.BE>
2016-12-09 11:35:04 +0100

    Portfolio

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -4,9 +4,7 @@ function main() {
       });
       $(function() {
             $("#slider").responsiveSlides(
-                  "timeout": 2000,          // Integer: Time between slide transitions, in milliseconds
                   "pager": true,           // Boolean: Show pager, true or false
-                  "nav": false,             // Boolean: Show navigation, true or false
                   "random": true,          // Boolean: Randomize the order of the slides, true or false
                   "pauseControls": true,    // Boolean: Pause when hovering controls, true or false
                   "namespace": "large-btns",   // String: change the default namespace used
