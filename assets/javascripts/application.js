f94d0363a5c37f5115f67f4ebea43df675b4c36c
EVA LASTERS <EVA.LASTERS@STUDENT.AP.BE>
2016-12-11 00:12:07 +0100

    Portfolio

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,19 +1,24 @@
 function main() {
-      $(function() {
-            $(".rslides").responsiveSlides();
-      });
-      $("#slider").responsiveSlides({
-            auto: true,
-            nav: true,
-            speed: 500,
-            pause: false,
-            namespace: "callbacks",
-            before: function () {
-                  $('.events').append("<li>before event fired.</li>");
-            },
-            after: function () {
-                  $('.events').append("<li>after event fired.</li>");
-            }
+      $(function () {
+        // Slideshow 3
+        $("#slider3").responsiveSlides({
+          manualControls: '#slider3-pager',
+          maxwidth: 540
+        });
+
+        // Slideshow 4
+        $("#slider3").responsiveSlides({
+          pager: false,
+          nav: true,
+          speed: 500,
+          namespace: "callbacks",
+          before: function () {
+            $('.events').append("<li>before event fired.</li>");
+          },
+          after: function () {
+            $('.events').append("<li>after event fired.</li>");
+          }
+        });
       });
 }
 
