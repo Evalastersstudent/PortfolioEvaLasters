dfd41edc2d871ab748a1e03407981793b1d44b81
EVA LASTERS <EVA.LASTERS@STUDENT.AP.BE>
2016-12-11 00:34:17 +0100

    Portfolio

--- _harp/assets/javascripts/application.js
+++ _harp/assets/javascripts/application.js
@@ -8,7 +8,14 @@ function main() {
 
         $("#slider3").responsiveSlides({
           nav: true,
-
+          speed: 1000,
+          namespace: "callbacks",
+          before: function () {
+            $('.events').append("<li>before event fired.</li>");
+          },
+          after: function () {
+            $('.events').append("<li>after event fired.</li>");
+          }
           prevText: "",
           nextText: ""
         });
